fd91372ed84254dd5614b6e4532b9bc82e10b000
Fahad1993 <foso1015@gmail.com>
2026-04-02 12:42:32 +0300

    Add auto-assign workflow

--- Tables/workOrder.sql
+++ Tables/workOrder.sql
@@ -147,3 +147,6 @@ CREATE TABLE [dbo].[WorkOrderTypeFields](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
+
+
+-- ============================================================================
