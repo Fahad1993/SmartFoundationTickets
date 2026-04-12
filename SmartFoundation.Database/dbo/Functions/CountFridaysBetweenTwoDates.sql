afe4e4ada54c09becfdc928e08afd35dcfc052cd
Abdulaziz Alfaifi <40849102+AbdulazizFF@users.noreply.github.com>
2026-04-12 10:53:33 +0300

    fixes: #125

--- SmartFoundation.Database/dbo/Functions/CountFridaysBetweenTwoDates.sql
+++ SmartFoundation.Database/dbo/Functions/CountFridaysBetweenTwoDates.sql
@@ -18,6 +18,7 @@ BEGIN
     SELECT @NumberOfFridays = (select count(*) from dbo._GregorianHijriDate gh
 	where 1=1
 	and CAST(gh.gregorianDate AS DATE) between @StartDate and @EndDate
+
 	and DATEPART(DW,CAST(gh.gregorianDate AS DATE)) =6); -- 6 indicates Friday (assuming the first day of the week is Sunday)
 
     RETURN @NumberOfFridays;
